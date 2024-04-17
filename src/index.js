7551c08dbf9e84b6d25977e270f1ac28b8925344
Caiouser2 <ocaio997@gmail.com>
2024-04-17 15:57:40 -0300

    fix gh-page not working

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import ShowUserInfo from './pages/ShowUserInfo/User';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path="simple-form/" element={<App/>}/>
             <Route path="/home" element={<ShowUserInfo/>}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
